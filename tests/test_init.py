b96a07e38737386ecc6714b47112c62d28b1ccc0
ngm1450 <georgeos.nicolas83@gmail.com>
2023-01-29 15:52:21 -0300

    atualização testes

---
+++ tests/test_init.py
@@ -0,0 +1,8 @@
+# Since there are no tests implemented, I create this dummy class that simulates them.
+
+def testGetGcesString():
+    result = getGcesString()
+    assert result == "GCES"
+
+def getGcesString():
+    return "GCES"
