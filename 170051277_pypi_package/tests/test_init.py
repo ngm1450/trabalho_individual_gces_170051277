f649d04c9b8610aa407dde7c85e1d45e3b775a76
ngm1450 <georgeos.nicolas83@gmail.com>
2023-01-29 19:03:35 -0300

    adição folder pacore pypi

--- 170051277_pypi_package/tests/test_init.py
+++ 170051277_pypi_package/tests/test_init.py
@@ -1,5 +1,7 @@
 # Since there are no tests implemented, I create this dummy class that simulates them.
 
+import pytest
+
 def testGetGcesString():
     result = getGcesString()
     assert result == "GCES"
